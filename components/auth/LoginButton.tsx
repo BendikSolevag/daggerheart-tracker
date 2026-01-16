d271c7da84e7bb1073d26c7ab48ac1b1e37e12e2
Baynrich <bendikasolevaag@gmail.com>
2026-01-16 18:10:20 +0100

    style

--- components/auth/LoginButton.tsx
+++ components/auth/LoginButton.tsx
@@ -7,9 +7,9 @@ function LoginButton() {
   const supabase = createClient();
 
   const [email, setEmail] = useState("");
+  const [sent, setSent] = useState(false);
 
   async function signInWithEmail() {
-    console.log("hamana hamana", email);
     const { data, error } = await supabase.auth.signInWithOtp({
       email: email,
       options: {
@@ -19,18 +19,24 @@ function LoginButton() {
       },
     });
     console.log(data, error);
+    setSent(true);
   }
   return (
     <>
-      <input className="border-2 border-lightblue px-2 py-1" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <button
-        onClick={() => {
-          signInWithEmail();
-        }}
-        className="block border-2 border-lightblue hover:bg-lightblue px-2 py-1 rounded-lg"
-      >
-        Sign in
-      </button>
+      {!sent && (
+        <>
+          <input className="border-2 border-lightblue px-2 py-1" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <button
+            onClick={() => {
+              signInWithEmail();
+            }}
+            className="block border-2 border-lightblue hover:bg-lightblue px-2 py-1 rounded-lg"
+          >
+            Sign in
+          </button>
+        </>
+      )}
+      {sent && <p>Du skal ha fått en e-post med en påloggingslenke nå. Sjekk søppelmappen også!</p>}
     </>
   );
 }
